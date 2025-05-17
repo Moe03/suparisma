1ed135485f40b98ff7af400a704640adf31e946e
Moe Ayman <mlotov2002@gmail.com>
2025-05-17 16:51:17 +0300

    ignroe gen files and updated read me

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+DO NOT INSTALL STILL UNDER CONSTRUCTION.
+
 This package combines supabase and prisma to easily generate realtime CRUD hooks for all your tables to be used seamlessly in react.
 
 To install first do:
--- src/generators/supabaseClientGenerator.ts
+++ src/generators/supabaseClientGenerator.ts
@@ -4,10 +4,8 @@ import { OUTPUT_DIR } from '../config'; // Assuming OUTPUT_DIR is defined in con
 
 export function generateSupabaseClientFile() {
   const supabaseClientContent = `// THIS FILE IS AUTO-GENERATED - DO NOT EDIT DIRECTLY
-import { createClient } from '@supabase/supabase-js';
+  // import { createClient } from '@supabase/supabase-js';
 
-console.log(\`NEXT_PUBLIC_SUPABASE_URL: \${process.env.NEXT_PUBLIC_SUPABASE_URL}\`);
-console.log(\`NEXT_PUBLIC_SUPABASE_ANON_KEY: \${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}\`);
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
